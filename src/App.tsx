7b554fb4a3158021071f6cb08c5ae651756c5df6
Alejandro Castillo <alejandrocastillo252006@gmail.com>
2025-06-23 14:22:35 -0400

    - Remove Aurora (low device performance reasons...)

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,14 @@ function App() {
     <AppContextProvider>
       <Header />
       <Main />
-
+{/* 
       <Aurora
         colorStops={["#1a4bdb", "#3e38e9", "#263abe"]}
         blend={0.1}
         amplitude={1.0}
         speed={0.3}
         className="w-full h-full fixed top-0 -z-1 bg-transparent opacity-50"
-      />
+      /> */}
       <Orb
         hoverIntensity={0.75}
         rotateOnHover={false}
