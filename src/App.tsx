fb20ca889c77e4a22e603e2838bbfaf1225f64ab
Alejandro Castillo <alejandrocastillo252006@gmail.com>
2025-06-24 11:30:07 -0400

    - Add more effect in the background

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,11 @@ function App() {
     <AppContextProvider>
       <Header />
       <Main />
-{/* 
-      <Aurora
-        colorStops={["#1a4bdb", "#3e38e9", "#263abe"]}
-        blend={0.1}
-        amplitude={1.0}
-        speed={0.3}
-        className="w-full h-full fixed top-0 -z-1 bg-transparent opacity-50"
-      /> */}
+
       <Orb
-        hoverIntensity={0.75}
-        rotateOnHover={false}
+        hoverIntensity={0.35}
+        rotateOnHover={true}
+        forceHoverState
         hue={45}
         className="fixed top-0 opacity-40 pointer-events-none -z-10"
       />
